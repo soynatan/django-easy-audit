2db183061c346c6a8b54af04f8c33dba0a724398
Jason Held <jasonsheld@gmail.com>
2017-01-06 11:45:38 -0500

    Extra configurability on what classes to watch for, based on the project.

---
+++ easyaudit/settings.py
@@ -0,0 +1,22 @@
+
+from django.conf import settings
+
+
+# django unregistered classes
+from django.db.migrations import Migration
+from django.contrib.admin.models import LogEntry
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+from easyaudit.models import CRUDEvent, LoginEvent
+
+
+# unregistered classes
+UNREGISTERED_CLASSES = [CRUDEvent, LoginEvent, Migration, LogEntry, Session, Permission, ContentType]
+# see if the project settings differ
+UNREGISTERED_CLASSES = getattr(settings, 'DJANGO_EASY_AUDIT_UNREGISTERED_CLASSES', UNREGISTERED_CLASSES)
+
+WATCH_LOGIN_EVENTS = getattr(settings, 'DJANGO_EASY_AUDIT_WATCH_LOGIN_EVENTS', True)
+
