320da42773a626bcaf334d9842839231ec068554
soynatan <natancalzolari@gmail.com>
2017-03-05 09:51:05 -0300

    Uses local threading to make request and user available globally

--- easyaudit/middleware/easyaudit.py
+++ easyaudit/middleware/easyaudit.py
@@ -1,13 +1,30 @@
 try:
     from django.utils.deprecation import MiddlewareMixin
-except:
+except ImportError:
     # not required in <= 1.9
     MiddlewareMixin = object
 
+# makes easy-audit thread-safe
+try:
+    from threading import local
+except ImportError:
+    from django.utils._threading_local import local
+
+_thread_locals = local()
+
+def get_current_request():
+    return getattr(_thread_locals, 'request', None)
+
+def get_current_user():
+    request = get_current_request()
+    if request:
+        return getattr(request, 'user', None)
+
 class EasyAuditMiddleware(MiddlewareMixin):
     """Makes request available to this app signals."""
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    request = None
-
-    def process_request(self, request):
-        EasyAuditMiddleware.request = request
+    def __call__(self, request):
+        _thread_locals.request = request
+        return self.get_response(request)
