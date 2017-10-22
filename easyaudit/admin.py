39fe96cc4862a130fe4c4ef181ffbd4283079fc9
Natán Calzolari <natancalzolari@gmail.com>
2017-10-22 11:09:36 -0300

    Added settings that allow the project to hide modules from Django admin.

--- easyaudit/admin.py
+++ easyaudit/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from . import models
-
+from . import models, settings
 
 # CRUD events
 class CRUDEventAdmin(admin.ModelAdmin):
     list_display = ['get_event_type_display', 'content_type', 'object_id', 'object_repr', 'user', 'datetime']
 
 
-admin.site.register(models.CRUDEvent, CRUDEventAdmin)
+if settings.ADMIN_SHOW_MODEL_EVENTS:
+    admin.site.register(models.CRUDEvent, CRUDEventAdmin)
 
 
 # Login events
@@ -15,7 +15,8 @@ class LoginEventAdmin(admin.ModelAdmin):
     list_display = ['datetime', 'get_login_type_display', 'user', 'username', 'remote_ip']
 
 
-admin.site.register(models.LoginEvent, LoginEventAdmin)
+if settings.ADMIN_SHOW_AUTH_EVENTS:
+    admin.site.register(models.LoginEvent, LoginEventAdmin)
 
 
 # Request events
@@ -23,4 +24,5 @@ class RequestEventAdmin(admin.ModelAdmin):
     list_display = ['datetime', 'user', 'method', 'url', 'remote_ip']
 
 
-admin.site.register(models.RequestEvent, RequestEventAdmin)
+if settings.ADMIN_SHOW_REQUEST_EVENTS:
+    admin.site.register(models.RequestEvent, RequestEventAdmin)
