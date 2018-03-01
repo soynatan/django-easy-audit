8ee195b9118b76b49eb71c54ae33dfd3124403b9
Natán Calzolari <natancalzolari@gmail.com>
2018-03-01 01:10:43 -0300

    Generic prettify_json function

--- easyaudit/admin.py
+++ easyaudit/admin.py
@@ -1,4 +1,3 @@
-import json
 from django.contrib import admin
 
 try: # Django 2.0
@@ -9,7 +8,7 @@ except: # Django < 2.0
 from django.utils.safestring import mark_safe
 from . import settings
 from .models import CRUDEvent, LoginEvent, RequestEvent
-from .admin_helpers import EasyAuditModelAdmin
+from .admin_helpers import prettify_json, EasyAuditModelAdmin
 
 
 # CRUD events
@@ -20,8 +19,8 @@ class CRUDEventAdmin(EasyAuditModelAdmin):
     search_fields = ['=object_id', 'object_json_repr', ]
     readonly_fields = ['event_type', 'object_id', 'content_type',
                        'object_repr', 'object_json_repr_prettified', 'user',
-                       'user_pk_as_string', 'datetime', 'changed_fields']
-    exclude = ['object_json_repr']
+                       'user_pk_as_string', 'datetime', 'changed_fields_prettified']
+    exclude = ['object_json_repr', 'changed_fields']
 
     def object_repr_link(self, obj):
         if obj.event_type == CRUDEvent.DELETE:
@@ -40,14 +39,14 @@ class CRUDEventAdmin(EasyAuditModelAdmin):
     object_repr_link.short_description = 'object repr'
 
     def object_json_repr_prettified(self, obj):
-        try:
-            data = json.loads(obj.object_json_repr)
-            html = '<pre>' + json.dumps(data, sort_keys=True, indent=4) + '</pre>'
-        except:
-            html = obj.object_json_repr
-        return mark_safe(html)
+        return prettify_json(obj.object_json_repr)
 
     object_json_repr_prettified.short_description = 'object json repr'
+
+    def changed_fields_prettified(self, obj):
+        return prettify_json(obj.changed_fields)
+
+    changed_fields_prettified.short_description = 'changed fields'
 
 
 if settings.ADMIN_SHOW_MODEL_EVENTS:
