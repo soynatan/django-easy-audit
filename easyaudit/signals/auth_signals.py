76d78b3d217c6eae2726d2f27ccddd94d5522f28
atakanarikan <marqhen@gmail.com>
2019-02-13 13:26:11 +0100

    use logging backend in auth_signals

--- easyaudit/signals/auth_signals.py
+++ easyaudit/signals/auth_signals.py
@@ -1,17 +1,22 @@
 from django.contrib.auth import signals, get_user_model
 from django.db import transaction
+from django.utils.module_loading import import_string
 
 from easyaudit.middleware.easyaudit import get_current_request
 from easyaudit.models import LoginEvent
-from easyaudit.settings import REMOTE_ADDR_HEADER, WATCH_AUTH_EVENTS
+from easyaudit.settings import REMOTE_ADDR_HEADER, WATCH_AUTH_EVENTS, LOGGING_BACKEND
+
+logger = import_string(LOGGING_BACKEND)()
+
 
 def user_logged_in(sender, request, user, **kwargs):
     try:
         with transaction.atomic():
-            login_event = LoginEvent.objects.create(login_type=LoginEvent.LOGIN,
-                                     username=getattr(user, user.USERNAME_FIELD),
-                                     user=user,
-                                     remote_ip=request.META[REMOTE_ADDR_HEADER])
+            logger.login({
+                'username': getattr(user, user.USERNAME_FIELD),
+                'user': user,
+                'remote_ip': request.META[REMOTE_ADDR_HEADER]
+            })
     except:
         pass
 
@@ -19,10 +24,12 @@ def user_logged_in(sender, request, user, **kwargs):
 def user_logged_out(sender, request, user, **kwargs):
     try:
         with transaction.atomic():
-            login_event = LoginEvent.objects.create(login_type=LoginEvent.LOGOUT,
-                                                    username=getattr(user, user.USERNAME_FIELD),
-                                                    user=user,
-                                                    remote_ip=request.META[REMOTE_ADDR_HEADER])
+            logger.login({
+                'login_type': LoginEvent.LOGOUT,
+                'username': getattr(user, user.USERNAME_FIELD),
+                'user': user,
+                'remote_ip': request.META[REMOTE_ADDR_HEADER]
+            })
     except:
         pass
 
@@ -30,11 +37,13 @@ def user_logged_out(sender, request, user, **kwargs):
 def user_login_failed(sender, credentials, **kwargs):
     try:
         with transaction.atomic():
-            request = get_current_request() # request argument not available in django < 1.11
+            request = get_current_request()  # request argument not available in django < 1.11
             user_model = get_user_model()
-            login_event = LoginEvent.objects.create(login_type=LoginEvent.FAILED,
-                                                    username=credentials[user_model.USERNAME_FIELD],
-                                                    remote_ip=request.META[REMOTE_ADDR_HEADER])
+            logger.login({
+                'login_type': LoginEvent.FAILED,
+                'username': credentials[user_model.USERNAME_FIELD],
+                'remote_ip': request.META[REMOTE_ADDR_HEADER]
+            })
     except:
         pass
 
