b5f5edccf90da1e31a6e0c0a4f4dc27b184dd678
James Kirsop <james.kirsop@gmail.com>
2019-07-17 13:04:02 +1000

    One liner for fix

--- easyaudit/signals/model_signals.py
+++ easyaudit/signals/model_signals.py
@@ -67,7 +67,7 @@ def pre_save(sender, instance, raw, using, update_fields, **kwargs):
             if not created:
                 old_model = sender.objects.get(pk=instance.pk)
                 delta = model_delta(old_model, instance)
-                changed_fields = json.dumps(delta)
+                changed_fields = json.dumps(delta) if delta else None
                 event_type = CRUDEvent.UPDATE
 
             # user
