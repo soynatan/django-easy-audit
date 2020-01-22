fa67b9d5e6d90941e6b2a535fd1f78c7f95c004c
Jason Held <jasonsheld@gmail.com>
2020-01-22 12:10:50 -0500

    version 1.2.1rc1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-easy-audit',
-    version='1.2',
+    version='1.2.1rc1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
