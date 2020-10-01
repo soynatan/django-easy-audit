7af5ac764749009b51beeae0c525c609669b34cc
Real-Gecko <alexandrbezenkov@gmail.com>
2020-10-01 15:43:50 +0600

    Replace deprecated ugettext_lazy with gettext_lazy

--- easyaudit/models.py
+++ easyaudit/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class CRUDEvent(models.Model):
