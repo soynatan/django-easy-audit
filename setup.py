89d48abd66eea66f33ab6df06684dcfc0dd127b5
Guglielmo Celata <97271+guglielmo@users.noreply.github.com>
2021-04-07 22:02:55 +0200

    install_requires list updated to support django 3.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "beautifulsoup4",
-        "django>=2.2,<3.2"
+        "django>=2.2,<3.3"
     ],
     python_requires=">=3.5",
     license='GPL3',
