fa86208acbeac66615f05ba22a9d1ded9f0a49ae
aymaneMx <aymanemx@gmail.com>
2022-02-23 03:59:43 +0100

    add default_auto_field to EasyAuditConfig

--- easyaudit/apps.py
+++ easyaudit/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 class EasyAuditConfig(AppConfig):
     name = 'easyaudit'
     verbose_name = 'Easy Audit Application'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         from easyaudit.signals import auth_signals, model_signals, request_signals
