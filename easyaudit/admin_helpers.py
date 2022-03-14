8a3d775ee2085b198217b2a528237fc14de11c1e
cybelew <thecybil@gmail.com>
2022-03-15 10:07:03 +1100

    Fix EasyAuditModelAdmin.has_delete_permission()
    EasyAuditModelAdmin.has_delete_permission() now respects user permissions if READONLY_EVENTS is False

--- easyaudit/admin_helpers.py
+++ easyaudit/admin_helpers.py
@@ -70,7 +70,7 @@ class EasyAuditModelAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         if settings.READONLY_EVENTS:
             return False
-        return True
+        return super().has_delete_permission(request, obj)
 
     def get_urls(self):
         info = self.model._meta.app_label, self.model._meta.model_name
