25cdd3ed181ca3667517c5f79073d6f3d2bca4a2
Kamil Niski <kamil.niski@ke-works.com>
2024-04-25 09:33:09 +0200

    Update EasyAuditMiddleware for async compatibility
    The EasyAuditMiddleware class has been updated to be compatible with both synchronous and asynchronous processes. The class initialization now checks if the 'get_response' function is a coroutine and sets the class' async capability accordingly. An '__acall__' method has been introduced to handle asynchronous calls.

--- easyaudit/middleware/easyaudit.py
+++ easyaudit/middleware/easyaudit.py
@@ -1,7 +1,11 @@
 # makes easy-audit thread-safe
-from asgiref.local import Local
 import contextlib
-from django.utils.deprecation import MiddlewareMixin
+from typing import Callable
+
+from asgiref.local import Local
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 
 
 class MockRequest:
@@ -38,28 +42,33 @@ def clear_request():
         del _thread_locals.request
 
 
-class EasyAuditMiddleware(MiddlewareMixin):
-    """Makes request available to this app signals."""
+class EasyAuditMiddleware:
+    async_capable = True
+    sync_capable = True
 
-    def __call__(self, request):
-        _thread_locals.request = (
-            request  # seems redundant w/process_request, but keeping in for now.
-        )
-        if hasattr(self, "process_request"):
-            response = self.process_request(request)
-        response = response or self.get_response(request)
-        if hasattr(self, "process_response"):
-            response = self.process_response(request, response)
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
+        self.get_response = get_response
+        if iscoroutinefunction(self.get_response):
+            markcoroutinefunction(self)
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        if iscoroutinefunction(self):
+            return self.__acall__(request)
+
+        _thread_locals.request = request
+        response = self.get_response(request)
+
+        with contextlib.suppress(AttributeError):
+            del _thread_locals.request
+
         return response
 
-    def process_request(self, request):
+    async def __acall__(self, request: HttpRequest) -> HttpResponse:
         _thread_locals.request = request
 
-    def process_response(self, request, response):
-        with contextlib.suppress(AttributeError):
-            del _thread_locals.request
-        return response
+        response = await self.get_response(request)
 
-    def process_exception(self, request, exception):
         with contextlib.suppress(AttributeError):
             del _thread_locals.request
+
+        return response
