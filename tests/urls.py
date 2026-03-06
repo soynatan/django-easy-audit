4f8712ffe9e429b2402030784fdf8ababd918be1
Paul Craciunoiu <pcraciunoiu@users.noreply.github.com>
2026-03-06 13:56:25 -0700

    Lint, actually add missing models.

--- tests/urls.py
+++ tests/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 
+
 """
+
 from django.contrib import admin
 from django.urls import include, re_path
 
